b43aab04e6cce1d2705b31bbc70c94b0b0c1ddf9
beq <blakebequette@gmail.com>
2022-12-12 09:58:06 -0600

    starting code

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const { models: { User }} = require('./db');
+const path = require('path');
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
+
+app.post('/api/auth', async(req, res, next)=> {
+  try {
+    res.send({ token: await User.authenticate(req.body)});
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.get('/api/auth', async(req, res, next)=> {
+  try {
+    res.send(await User.byToken(req.headers.authorization));
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+// error handling
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.status || 500).send({ error: err.message });
+});
+
+module.exports = app;
