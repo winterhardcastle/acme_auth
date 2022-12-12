0daa2fc28c58949334ef1298bc054d69e0378292
Winter Hardcastle <winter@hardcastle.io>
2022-12-12 14:56:44 -0500

    Finished Assignment

--- app.js
+++ app.js
@@ -9,31 +9,43 @@ const env = require("dotenv").config();
 // middleware
 app.use(express.json());
 
+const requireToken = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    const user = await User.byToken(token);
+    req.user = user;
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
+
 // routes
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
 app.post("/api/auth", async (req, res, next) => {
   try {
-    res.send({ token: await User.authenticate(req.body) });
+    res.send({ authorization: await User.authenticate(req.body) });
   } catch (ex) {
     next(ex);
   }
 });
 
-app.get("/api/auth", async (req, res, next) => {
+app.get("/api/auth", requireToken, async (req, res, next) => {
   try {
-    res.send(await User.byToken(req.headers.authorization));
+    res.send(req.user);
   } catch (ex) {
     next(ex);
   }
 });
 
-app.get("/api/users/:id/notes", async (req, res, next) => {
+app.get("/api/users/:id/notes", requireToken, async (req, res, next) => {
   try {
     const id = req.params.id;
-    const { token }= req.headers;
-    const user = await User.byToken(token)
-    if (user.id != id) throw new Error("token does not match route")
+    // const { token } = req.headers;
+    // const user = await User.byToken(token);
+    const user = req.user;
+    if (user.id != id) throw new Error("token does not match route");
     res.send(
       await Note.findAll({
         where: { userId: id },
